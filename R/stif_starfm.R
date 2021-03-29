afa6e481134c11051c5f655d03b2624bda40b46d
mmontesinosanmartin <montesino.upna@gmail.com>
2021-03-29 17:53:51 +0200

    fix STARFM small bug

--- R/stif_starfm.R
+++ R/stif_starfm.R
@@ -5,18 +5,18 @@
 #' \eqn{t_1} and \eqn{t_2}.
 #' 
 #' This is a fast implementation of the STARFM algorithm for the specific
-#' situation a single pair of images is used to predict the fine scale image
-#' at t_2.
+#' situation in which a single pair of images is used to predict the fine
+#' scale image at \eqn{t_2}.
 #' 
-#' @param c1 a coarse image from t1 as a \code{RasterStack}.
-#' @param c2 a coarse image from t2 as a \code{RasterStack}.
-#' @param f1 a fine image from t1 as a \code{RasterStack}.
-#' @param red.band integer. The number of the red band (default, 1).
-#' @param nir.band integer. The number of the NIR band (default, 4).
-#' @param w integer. The radius of the neighboring area (default, 20).
-#' @param ns integer. Number of similar pixels considered for the spatial weighting (default, 20).
+#' @param c1 a coarse image from t1 as a \code{RasterStack}
+#' @param c2 a coarse image from t2 as a \code{RasterStack}
+#' @param f1 a fine image from t1 as a \code{RasterStack}
+#' @param red.band integer. The number of the red band (default, 1)
+#' @param nir.band integer. The number of the NIR band (default, 4)
+#' @param w integer. The radius of the neighboring area (default, 20)
+#' @param ns integer. Number of similar pixels considered for the spatial weighting (default, 20)
 #' @param scale the dynamic range of the image (default, \code{c(0,1)}
-#' @param verbose whether to notify intermediate steps (default, \code{TRUE)}.
+#' @param verbose whether to notify intermediate steps (default, \code{TRUE)}
 #' 
 #' @return the fine image predicted at t2 as a \code{RasterStack}
 #' 
@@ -42,8 +42,9 @@ stif_starfm <- function(c1,
   
   # spatial weighting
   out <- .gen_tmpl(raster(f1), nlayers(f1))
-  out[] <- starfm_spatial_filter_fast(c1 = c1.intp[], c2 = c2.intp[], f1 = f1[],
-                                       dims = dim(f1), redb = 1, nirb = 4, w0, ns)
+  out[] <- starfm_spatial_filter(c1 = c1.intp[], c2 = c2.intp[], f1 = f1[],
+                                 dims = dim(f1), redb = red.band,
+                                 nirb = nir.band, w, ns)
   
   # changing format
   out <- clamp(out, min(scale), max(scale))
@@ -51,13 +52,10 @@ stif_starfm <- function(c1,
   return(out)
 }
 
-# helper functions
 .raster_warp <- function(r, ref, method, use_gdal){
   as(st_warp(st_as_stars(r), st_as_stars(ref), method, use_gdal = use_gdal), "Raster")
 }
-.m2r <- function(m){
-  raster(m)[]
-}
+
 .gen_tmpl <- function(tmpl, nlyr){
   tmpl[] <- NA
   stack(lapply(1:nlyr, function(i, r) r, r = tmpl))
